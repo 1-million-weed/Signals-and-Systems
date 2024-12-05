99a70a01fb57cdbd05134c9814243383a14b90eb
Marinus Van Den Ende <m3vdende@gmail.com>
2024-12-05 17:09:04 +0100

    Sum of Sinoids

--- Assignments/Assignment 1/template.py
+++ Assignments/Assignment 1/template.py
@@ -1,4 +1,4 @@
-import math
+import math, cmath
 
 def get_input_data():
     input_1, input_2, input_3 = map(int, input().split())
@@ -17,14 +17,27 @@ def print_output_list(output_list):
 def cartesian_to_polar(x,y) -> tuple:
     """
     Convert cartesian coordinates to polar coordinates.
+
+    r = √(x2 + y2) (Length) (A.5a) \n
+    θ = arctan(y/x) (Direction) (A.5b) \n
+
+    z = re^(jθ) = |z|e^(j arg z)
+    where |z| = r = √(x2 + y2) is called the magnitude of the vector 
+    and arg z = θ is its phase in radians (not degrees)
+
+    TODO: However, we generally specify the principal value of the angle 
+    so that −180◦ < θ ≤ 180◦. This requires that integer multiples 
+    of 360◦ be subtracted from or added to the angle until the result 
+    is between −180◦ and +180◦. Thus, the vector 3∠−80◦ is the principal 
+    value of 3∠280◦.
     
     Args:
-        - x (float) : x-coordinate
-        - y (float) : y-coordinate
+        x (float) x-coordinate.
+        y (float) y-coordinate.
 
     Returns:
-        - r (float) : radius
-    - phi (float) : angle in radians
+        r (float): radius
+        phi (float): angle in radians
     """
     # transform coordinates
     r = float(math.sqrt( (x*x) + (y*y) ))
@@ -48,13 +61,19 @@ def polar_to_cartesian(r, phi) -> tuple:
     """
     Convert polar coordinates to cartesian coordinates.
 
+    x = r cos θ (A.3a) \n
+    y = r sin θ (A.3b) \n
+    z = r cos θ + j r sin θ (A.4)
+
+    Euler’s Formula: \n
+    e^(jθ) = cos θ + j sin θ (A.7)
+
     Args:
-    - r (float) : radius
-    - phi (float) : angle in radians
+        r (float): The radius.
+        phi (float): The angle in radians.
 
     Returns:
-    - x (float) : x-coordinate
-    - y (float) : y-coordinate
+        tuple: A tuple containing the x and y coordinates.
     """
     # transform coordinates
     x = r*math.cos(phi)
@@ -66,3 +85,30 @@ def polar_to_cartesian(r, phi) -> tuple:
     
     #return cartesian
     return x, y
+
+def sum_of_sinoids() -> float:
+    # input: 42 2 
+    # 1 1.047198 
+    # 1 0.523599 
+    # output: x(t)=1.93cos(2*pi*42*t+0.79)
+    # get the frequency and number of sinoids
+    f, n = map(int, input().split())
+
+    a_sum = 0
+
+    # sum of phasors
+    for _ in range(n):
+        # get the amplitude and phase
+        a, phase = map(float, input().split())
+        a_sum += a * cmath.exp(1j * phase)
+
+    # polar form
+    amp = round(abs(a_sum), 2)
+    phase = round(cmath.phase(a_sum), 2)
+
+    # output
+    if a == 0:
+        return "x(t)=0.00"
+    else:
+        return "x(t)={}cos(2*pi*{}*{}*t+{})".format(amp, f, n, phase)
+
